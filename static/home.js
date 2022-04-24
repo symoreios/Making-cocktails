1e4b2279649920d70e8cb8d0859b28ee9906b630
yaolin107 <yl4390@columbia.edu>
2022-04-24 14:11:29 -0400

    modified homepage to look nice based on wednesday lecture

--- static/home.js
+++ static/home.js
@@ -2,6 +2,10 @@ function display_home(){
     $("#home-page-btn").click(function(){
         window.location.assign("/" + "learn/" + "1")
     })
+
+    $("#home-page-quiz-btn").click(function(){
+        window.location.assign("/quiz/1")
+    })
 }
 
 $(document).ready(function(){
