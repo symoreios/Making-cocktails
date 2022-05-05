8454b37ea2d57586b7f47c3e8024fb2c709d122c
deji725 <deji725@gmail.com>
2022-05-05 18:25:50 -0400

    standardized review page cards and images, plus increased the margins

--- static/review.js
+++ static/review.js
@@ -2,6 +2,8 @@ function display_overview_data(data,review) {
 
      for(let i = 0; i < review.length; i++) {
         let row_1 = $("<div class='col-md-6'>")
+        let inner_col_1 = $("<span class='col-md-6'>")
+        let inner_column_2 = $("<span class='col-md-6 review-tag-text'>")
         let button_1 = $('<button />').attr({
             'title': 'review button',
             'class': 'btn btn-outline-warning btn-lg review-tag',
@@ -15,20 +17,23 @@ function display_overview_data(data,review) {
                 'src': data[review[i]]["picture"],
                 'alt': 'overview media',
                 'title': 'learning content media',
-                'class': 'img-fluid overview-media-1',
+                'class': 'review-image-tag',
             })
-            $(button_1).append(button_media_1)
+            $(inner_col_1).append(button_media_1)
+            $(button_1).append(inner_col_1)
         }else{
     
             let button_media_1 = $('<img />').attr({
                 'src': data[review[i]]["media"],
                 'alt': 'overview media',
                 'title': 'learning content media',
-                'class': 'img-fluid overview-media-1',
+                'class': 'review-image-tag',
             })
-            $(button_1).append(button_media_1)
+            $(inner_col_1).append(button_media_1)
+            $(button_1).append(inner_col_1)
         }
-        $(button_1).append(data[review[i]]["title"])
+        $(inner_column_2).append(data[review[i]]["title"])
+        $(button_1).append(inner_column_2)
         $(button_link_1).append(button_1)
         $(row_1).append(button_link_1)
         $(".review-specific").append(row_1)
